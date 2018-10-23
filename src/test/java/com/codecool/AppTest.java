1af3a3ddd12b691bdd261f77b968b0f8be9bc379
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2018-10-23 09:08:28 +0200

    Add IO test

---
+++ src/test/java/com/codecool/AppTest.java
@@ -0,0 +1,31 @@
+package com.codecool;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void shouldTakeUserInput() {
+
+        //Arrange
+        String input = "akka";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+
+        //Act
+        App app = new App();
+        app.main(new String[0]);
+
+        //Assert
+        String expected = "akka is a palindrome.";
+
+        assertEquals(expected, myOut.toString());
+    }
+}
